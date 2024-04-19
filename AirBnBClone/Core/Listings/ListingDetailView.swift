8852fb6e432e751cdfb0ae1f1506fca2c46eaea5
Pankaj Sachdeva <dearestpankaj@gmail.com>
2024-04-19 11:23:32 +0200

    Simplify main view by moving the content to separate variables

--- AirBnBClone/Core/Listings/ListingDetailView.swift
+++ AirBnBClone/Core/Listings/ListingDetailView.swift
@@ -41,138 +41,27 @@ struct ListingDetailView: View {
                 }.frame(height: 400)
                 
                 Section {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text(listing.title)
-                            .font(.title)
-                            .fontWeight(.semibold)
-                        VStack(alignment: .leading) {
-                            HStack(spacing: 2) {
-                                Image(systemName: "star.fill")
-                                
-                                Text("\(listing.rating)")
-                                Text(" - ")
-                                Text("28 reviews")
-                                    .underline()
-                                    .fontWeight(.semibold)
-                            }
-                            .foregroundStyle(.black)
-                            Text("\(listing.city), \(listing.state)")
-                        }
-                        .font(.caption)
-                    }
-                    .padding(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    propertyTitle
                     
                     Divider()
                     
-                    HStack {
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text("Entire \(listing.type.description) hosted by \(listing.ownerName)")
-                                .font(.headline)
-                                .frame(width: 250, alignment: .leading)
-                            
-                            HStack(spacing: 2) {
-                                Text("\(listing.numberOfGuests) guests -")
-                                Text("\(listing.numberOfBedrooms) bedrooms -")
-                                Text("\(listing.numberOfBeds) beds -")
-                                Text("\(listing.numberOfBathrooms) baths -")
-                            }
-                            .font(.caption)
-                        }
-                        .frame(width: 300, alignment: .leading)
-                        
-                        Spacer()
-                        
-                        Image(listing.ownerImageUrl)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 64, height: 64)
-                            .clipShape(Circle())
-                    }
-                    .padding()
+                    hostInfo
                     
                     Divider()
                     
-                    VStack(alignment: .leading, spacing: 16) {
-                        ForEach(listing.features) { feature in
-                            HStack(spacing: 12) {
-                                Image(systemName: feature.imageName)
-                                
-                                VStack(alignment: .leading) {
-                                    Text(feature.title)
-                                        .font(.footnote)
-                                        .fontWeight(.semibold)
-                                    
-                                    Text(feature.subTitle)
-                                        .font(.caption)
-                                        .foregroundStyle(.gray)
-                                }
-                                Spacer()
-                            }
-                        }
-                    }
-                    .padding()
+                    listingInfo
                     
                     Divider()
                     
-                    VStack(alignment: .leading, spacing: 16) {
-                        Text("Where you'll sleep")
-                            .font(.headline)
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 16, content: {
-                                ForEach(1 ... listing.numberOfBedrooms, id: \.self) { bedroom in
-                                    VStack {
-                                        Image(systemName: "bed.double")
-                                        
-                                        Text("Bedroom \(bedroom)")
-                                    }
-                                    .frame(width: 132, height: 100)
-                                    .overlay {
-                                        RoundedRectangle(cornerRadius: 12)
-                                            .stroke(lineWidth: 1)
-                                            .foregroundStyle(.gray)
-                                        
-                                    }
-                                }
-                            })
-                        }
-                        .scrollTargetBehavior(.paging)
-                    }
-                    .padding()
+                    bedInfo
                     
                     Divider()
                     
-                    VStack(alignment: .leading, spacing: 16) {
-                        Text("What this place offers")
-                            .font(.headline)
-                        
-                        ForEach(listing.amenities) { amenity in
-                            HStack{
-                                Image(systemName: amenity.imageName)
-                                    .frame(width: 32)
-                                
-                                Text(amenity.title)
-                                
-                                Spacer()
-                            }
-                        }
-                    }
-                    .padding()
+                    amenitiesView
                     
                     Divider()
                     
-                    VStack(alignment: .leading, spacing: 16, content: {
-                        Text("Where you'll be")
-                            .font(.headline)
-                        
-                        Map(position: $cameraPosition)
-                            .frame(height: 200)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                    })
-                    .padding()
-                } header: {
-                    
-                    
+                    mapView
                 }
                 
             })
@@ -181,37 +70,173 @@ struct ListingDetailView: View {
         .ignoresSafeArea()
         .padding(.bottom, 64)
         .overlay(alignment: .bottom) {
-            VStack {
-                Divider()
-                    .padding(.bottom)
-                HStack {
+            bottomPriceView
+        }
+    }
+    
+    var propertyTitle: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(listing.title)
+                .font(.title)
+                .fontWeight(.semibold)
+            VStack(alignment: .leading) {
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("\(listing.rating)")
+                    Text(" - ")
+                    Text("28 reviews")
+                        .underline()
+                        .fontWeight(.semibold)
+                }
+                .foregroundStyle(.black)
+                Text("\(listing.city), \(listing.state)")
+            }
+            .font(.caption)
+        }
+        .padding(.leading)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    var hostInfo: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Entire \(listing.type.description) hosted by \(listing.ownerName)")
+                    .font(.headline)
+                    .frame(width: 250, alignment: .leading)
+                
+                HStack(spacing: 2) {
+                    Text("\(listing.numberOfGuests) guests -")
+                    Text("\(listing.numberOfBedrooms) bedrooms -")
+                    Text("\(listing.numberOfBeds) beds -")
+                    Text("\(listing.numberOfBathrooms) baths -")
+                }
+                .font(.caption)
+            }
+            .frame(width: 300, alignment: .leading)
+            
+            Spacer()
+            
+            Image(listing.ownerImageUrl)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 64, height: 64)
+                .clipShape(Circle())
+        }
+        .padding()
+    }
+    
+    var listingInfo: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            ForEach(listing.features) { feature in
+                HStack(spacing: 12) {
+                    Image(systemName: feature.imageName)
+                    
                     VStack(alignment: .leading) {
-                        Text("\(listing.pricePerNight)€")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                        
-                        Text("Total before taxes")
-                        
-                        Text("Oct 15 - 20")
+                        Text(feature.title)
                             .font(.footnote)
                             .fontWeight(.semibold)
-                            .underline()
+                        
+                        Text(feature.subTitle)
+                            .font(.caption)
+                            .foregroundStyle(.gray)
                     }
                     Spacer()
-                    Button {
-                        
-                    } label: {
-                        Text("Reserve")
-                            .foregroundStyle(.white)
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .frame(width: 140, height: 40)
-                            .background(.pink)
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                    }
                 }
-                .padding(.horizontal, 32)
             }
+        }
+        .padding()
+    }
+    
+    var bedInfo: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Where you'll sleep")
+                .font(.headline)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 16, content: {
+                    ForEach(1 ... listing.numberOfBedrooms, id: \.self) { bedroom in
+                        VStack {
+                            Image(systemName: "bed.double")
+                            
+                            Text("Bedroom \(bedroom)")
+                        }
+                        .frame(width: 132, height: 100)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(lineWidth: 1)
+                                .foregroundStyle(.gray)
+                            
+                        }
+                    }
+                })
+            }
+            .scrollTargetBehavior(.paging)
+        }
+        .padding()
+    }
+    
+    var amenitiesView: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("What this place offers")
+                .font(.headline)
+            
+            ForEach(listing.amenities) { amenity in
+                HStack{
+                    Image(systemName: amenity.imageName)
+                        .frame(width: 32)
+                    
+                    Text(amenity.title)
+                    
+                    Spacer()
+                }
+            }
+        }
+        .padding()
+    }
+    
+    var mapView: some View {
+        VStack(alignment: .leading, spacing: 16, content: {
+            Text("Where you'll be")
+                .font(.headline)
+            
+            Map(position: $cameraPosition)
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+        })
+        .padding()
+    }
+    
+    var bottomPriceView: some View {
+        VStack {
+            Divider()
+                .padding(.bottom)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("\(listing.pricePerNight)€")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                    
+                    Text("Total before taxes")
+                    
+                    Text("Oct 15 - 20")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .underline()
+                }
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Reserve")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 140, height: 40)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+            }
+            .padding(.horizontal, 32)
         }
     }
 }
